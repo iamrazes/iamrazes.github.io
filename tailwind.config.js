748c4cf7307b1930f4539009367c33cb6e83df98
Muhammad Dagistan Silawane (Razes) <71701718+iamrazes@users.noreply.github.com>
2024-02-28 08:48:21 +0700

    update 14

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#3577b3',
+      },
+    },
   },
   plugins: [],
 }
