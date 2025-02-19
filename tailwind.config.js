abf557f880ed4683d3a3ed2653e28058b5c5cb08
Muhammad Dagistan Silawane (Razes) <71701718+iamrazes@users.noreply.github.com>
2025-02-19 11:37:46 +0700

    header done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'basecolor': '#2A3543',
+        'accent-1': '#4991EC',
+        'accent-2': '#80B8FF',
+        'accent-3': '#2E69B4',
+      },
+    },
+  },
+  plugins: [],
+}
+
